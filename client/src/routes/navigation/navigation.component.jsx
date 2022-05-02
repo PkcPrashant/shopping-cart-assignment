fbce2ebd668db01dd5dae3f7e5a75cef0768404b
Prashant Choudhary <prachoud4@publicisgroupe.net>
2022-05-02 22:31:22 +0530

    styling changes

--- client/src/routes/navigation/navigation.component.jsx
+++ client/src/routes/navigation/navigation.component.jsx
@@ -8,13 +8,13 @@ function Navigation() {
             <div>
               <img src="/static/images/logo_2x.png" className="w-50" alt="Sabka Bazaar" />
             </div>
-            <div className="flex-grow-1">
+            <div className="flex-grow-1 align-self-center">
               <Link className="link" to='/'>Home</Link>
               <Link className="link" to='/products'>Products</Link>
             </div>
-            <div className="d-flex align-items-end justify-content-end">
-              <img src="/static/images/cart.svg" className="w-10" alt="Cart Items" />
-              0 Items
+            <div className="d-flex align-self-end align-items-center justify-content-center w-12 cart">
+              <img src="/static/images/cart.svg" className="w-30" alt="Cart Items" />
+              <span>0 Items</span>
             </div>
         </div>
         <Outlet />
