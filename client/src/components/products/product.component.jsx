cd6e3b272e2d2e857ae838b305fcf7cd4cd19a1a
Prashant Choudhary <prachoud4@publicisgroupe.net>
2022-05-02 22:03:09 +0530

    styling changes

--- client/src/components/products/product.component.jsx
+++ client/src/components/products/product.component.jsx
@@ -20,7 +20,7 @@ function Product({ product }) {
     }
 
     return (
-        <div className="w-20 p-3 m-2 shadow">
+        <div className="w-23 p-3 m-2 shadow">
             <p className="bold">{product.name}</p>
             <img src={product.imageURL} alt={product.name} className="w-100" height="150px" />
             <p className="text">{product.description}</p>
