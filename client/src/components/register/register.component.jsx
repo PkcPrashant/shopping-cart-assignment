56bb64d7a13705ae481762fb55a7b6d00d96f88d
Prashant Choudhary <prachoud4@publicisgroupe.net>
2022-05-04 16:23:26 +0530

    validations

--- client/src/components/register/register.component.jsx
+++ client/src/components/register/register.component.jsx
@@ -1,11 +1,39 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import { useNavigate } from 'react-router-dom';
 import './register.css';
 
+const defaultFormFields = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: '',
+    confirmPassword: ''
+}
 
 function Register() {
 
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState(defaultFormFields);
+    const [error, setError] = useState('');
+    const { firstName, lastName, email, password, confirmPassword } = formData;
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        if (password !== confirmPassword) {
+            setError('Passwords do not match');
+        } else if (password.length < 6) {
+            setError('Password must have 6 characters');
+        } else {
+            navigate('/');
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
 
     return (
         <div className="d-flex flex-row justify-content-around m-5">
@@ -13,16 +41,19 @@ function Register() {
                 <p className='bold'>Signup</p>
                 <p>We do not share your personal details with anyone</p>
             </div>
-            <form className="d-flex flex-column w-25">
-                    <input type="text" className="input-fields" required placeholder='First Name' />
-                    <input type="text" className="input-fields" required placeholder='Last Name' />
-                    <input type="email" className="input-fields" required placeholder='Email' />
-                    <input type="password" className="input-fields" required placeholder='Password' />
-                    <input type="password" className="input-fields" required placeholder='Confirm Password' />
-                <div>
-                    <Button className='w-100' onClick={() => navigate('/')}>Signup</Button>
-                </div>
-            </form>
+            <div className="d-flex flex-column w-25">
+                <p className='bold error'>{error}</p>
+                <form onSubmit={submitForm}>
+                    <input type="text" className="input-fields" required placeholder='First Name' value={firstName} name='firstName' onChange={handleChange} />
+                    <input type="text" className="input-fields" required placeholder='Last Name' value={lastName} name='lastName' onChange={handleChange} />
+                    <input type="email" className="input-fields" required placeholder='Email' value={email} name='email' onChange={handleChange} />
+                    <input type="password" className="input-fields" required placeholder='Password' value={password} name='password' onChange={handleChange} />
+                    <input type="password" className="input-fields" required placeholder='Confirm Password' value={confirmPassword} name='confirmPassword' onChange={handleChange} />
+                    <div>
+                        <Button className='w-100' type='submit'>Signup</Button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/components/signin/signin.component.jsx
+++ client/src/components/signin/signin.component.jsx
@@ -1,10 +1,29 @@
 import Button from 'react-bootstrap/Button';
 import './signin.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
+const defaultFormFields = {
+    email: '',
+    password: ''
+}
 
 function SignIn() {
 
     const navigate = useNavigate();
+
+    const [formData, setFormData] = useState(defaultFormFields);
+    const { email, password } = formData;
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        navigate('/')
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
 
     return (
         <div className="d-flex flex-row justify-content-around m-5">
@@ -12,11 +31,11 @@ function SignIn() {
                 <p className='bold'>Login</p>
                 <p>Get access to your Orders, Wishlist and Recommendations</p>
             </div>
-            <form className="d-flex flex-column w-25">
-                    <input type="email" className="input-fields" required placeholder='Email' />
-                    <input type="password" className="input-fields" required placeholder='Password' />
+            <form className="d-flex flex-column w-25" onSubmit={submitForm}>
+                <input type="email" value={email} className="input-fields" placeholder='Email' required name='email' onChange={handleChange} />
+                <input type="password" value={password} className="input-fields" placeholder='Password' required name='password' onChange={handleChange} />
                 <div>
-                    <Button className='w-100' onClick={() => navigate('/')}>Login</Button>
+                    <Button className='w-100' type='submit'>Login</Button>
                 </div>
             </form>
         </div>
